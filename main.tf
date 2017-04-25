a0a24409ec06863d2c2d6a3cd104cce1df10270c
Carlos María Cornejo Crespo <cmcornejo@atsistemas.com>
2017-04-25 16:55:20 +0200

    Added subnet

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ resource "aws_vpc" "demo" {
   }
 }
 
+# Create a subnet to launch our instances into
+resource "aws_subnet" "demo" {
+  vpc_id                  = "${aws_vpc.demo.id}"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags {
+    Name = "subnet-enmilocalfunciona"
+  }
+}
+
+
